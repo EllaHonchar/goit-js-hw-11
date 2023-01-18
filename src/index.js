4c8aa52c0e84a0b515e156ac0181d509a0c1e631
EllaHonchar <kuchincus82@gmail.com>
2023-01-19 01:30:42 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,72 @@
 import { PixabayAPI } from "./pixabay-api";
 import { createCardsGallery } from "./cards"; 
-import Notiflix from "notiflix";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const searchInput = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
+const searchBtn = document.querySelector('.btn');
 
 const pixabayAPI  = new PixabayAPI();
-
-console.log(pixabayAPI)
-
+//console.log(pixabayAPI)
 searchInput.addEventListener('submit', onSearchFormButtonClick);
 
 function onSearchFormButtonClick(event) {
     event.preventDefault();
 
+    searchBtn.disabled = true;
+
     pixabayAPI.query = event.target.elements.searchQuery.value.trim();
-    
+    pixabayAPI.page = 1;
+
     pixabayAPI.fetchPhotosByQuery()
     .then( data => {
+        if (data.hits.length === 0) {
+            Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            event.target.reset();
+            // gallery.innerHTML = '';
+
+            return;
+        }
+
+        if (data.totalHits > 40) {
+            loadMoreButton.classList.remove('is-hidden');
+        }
+
         gallery.innerHTML = createCardsGallery(data.hits);
-    
-        loadMoreButton.classList.remove('is-hidden');
     })
     .catch(err => {
         console.log(err)
+    })
+    .finally(() => {
+        searchBtn.disabled = false;
     })
 }
 
 loadMoreButton.addEventListener('click', onLoadMoreBtnClick);
 
 function onLoadMoreBtnClick(event) {
-console.log('hello')
-}
+    pixabayAPI.page += 1;
+
+    pixabayAPI
+    .fetchPhotosByQuery()
+    .then(data => {
+        gallery.insertAdjacentHTML(
+            'beforeend', 
+            createCardsGallery(data.hits)
+            );
+
+        if (data.totalHits === pixabayAPI.hits) {
+            loadMoreButton.classList.add('is-hidden')
+        }
+
+    })
+    .catch(err => {
+        console.log(err)
+    });
+};
 
 
 
